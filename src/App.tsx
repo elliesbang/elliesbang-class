1443fbaec98e572978a64a4f9ec4f7968e70386f
elliesbang <shandbr1996@gmail.com>
2025-12-05 21:01:45 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ import FeedbackPage from "./page/admin/FeedbackPage";
 import GlobalNotices from "./page/admin/notices/GlobalNotices";
 import VodManager from "./page/admin/vod/VodManager";
 
-import UserManage from "./page/admin/users/UserManage";
+import StudentUsers from "./page/admin/users/StudentUsers";
 import VodUsers from "./page/admin/users/VodUsers";
 
 import ClassManager from "./page/admin/classes/ClassManager";
