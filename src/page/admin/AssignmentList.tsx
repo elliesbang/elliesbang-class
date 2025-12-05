da867dbc5a5499dcc3182a1a1a31523d4bbc42b6
elliesbang <shandbr1996@gmail.com>
2025-12-05 20:34:53 +0900

    Create AssignmentList.tsx

---
+++ src/page/admin/AssignmentList.tsx
@@ -0,0 +1,8 @@
+export default function AssignmentList() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Assignment List</h1>
+      <p>관리자용 과제 리스트 페이지 (임시 템플릿)</p>
+    </div>
+  );
+}
