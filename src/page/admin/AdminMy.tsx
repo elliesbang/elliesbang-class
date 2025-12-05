101002ffd7fcda20e0e0b8b63cebf32926c21505
elliesbang <shandbr1996@gmail.com>
2025-12-05 17:07:54 +0900

    Create AdminMy.tsx

---
+++ src/page/admin/AdminMy.tsx
@@ -0,0 +1,10 @@
+const AdminMy = () => {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>관리자 마이</h1>
+      <p>관리자 마이 페이지입니다. (v2 기본 화면)</p>
+    </div>
+  );
+};
+
+export default AdminMy;
